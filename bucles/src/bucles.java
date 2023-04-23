002b4039f7af432410921330547b4459451d6e4a
Jokunaxu <131497065+Jokunaxu@users.noreply.github.com>
2023-04-23 10:58:14 -0400

    Add files via upload

---
+++ bucles/src/bucles.java
@@ -0,0 +1,19 @@
+public class bucles {
+
+    public static void main(String[] args) {
+        int contador = 5;
+
+        while (contador <= 12){
+            System.out.println("sumando en bucle: "+ contador);
+            contador = contador +1;
+        }
+        int contador2 = 12;
+
+        while (contador2 >= 1){
+            System.out.println("restando numeros en bucle: "+ contador2);
+            contador2 = contador2 -1;
+    }
+
+        }
+
+}
